8381f7cb7cfbd9c2cba8dbc14cdd1f5490e1e824
Oluwaphemmy <ojokofemi@gmail.com>
2022-10-06 01:01:01 +0100

    new

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        body: "#ffffff",
+        cont: "#132B50",
+        enter: "#132b50",
+        bord: "#132b50",
+      },
+    },
+  },
+  plugins: [],
+};
